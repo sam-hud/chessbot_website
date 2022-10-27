f1cfba26840655c8345be241454f75c0ae05a1f1
Sam Hudson <sam.hud@icloud.com>
2022-10-27 13:48:47 -0700

    Initial commit

---
+++ src/main/java/me507/chessbot/ChessbotSiteApplication.java
@@ -0,0 +1,13 @@
+package me507.chessbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessbotSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessbotSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me507/chessbot/ChessbotSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package me507.chessbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessbotSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
